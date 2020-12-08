fe45cf16e6c2a0d8871a8cc274a89fba0797269d
Euripedes Souza <72283586+Euripedes0097@users.noreply.github.com>
2020-12-07 22:30:58 -0300

    Correção dos comentários

--- numeros_primos.py
+++ numeros_primos.py
@@ -1,15 +1,15 @@
-#O programa otimiza o número verificações entre os possíveis divisores e cada número.
-#Ao invés de verificar todos os números inferiores, basta começar a verificar a partir da raiz quadrada inteira.
-#Se um número for um quadrado perfeito, a sua raiz quadrada será o máximo divisor comum, então já é possível
-#desconsiderá-lo como um número primo e não necessário analisar os outros números maiores ou menores que ele.
-#Caso ele não seja um quadrado perfeito, a sua raiz quadrada inteira 'r' será suficiente para o intervalo [2, r] pertencente ao
-#conjunto dos naturais conferir se este número em questão é um número primo.
+"""O programa otimiza o número verificações entre os possíveis divisores e cada número.
+Ao invés de verificar todos os números inferiores, basta começar a verificar a partir da raiz quadrada inteira.
+Se um número for um quadrado perfeito, a sua raiz quadrada será o máximo divisor comum, então já é possível
+desconsiderá-lo como um número primo e não necessário analisar os outros números maiores ou menores que ele.
+Caso ele não seja um quadrado perfeito, a sua raiz quadrada inteira 'r' será suficiente para o intervalo [2, r] pertencente ao
+conjunto dos naturais conferir se este número em questão é um número primo."""
 
 import math
 
 primos = [2]
 
-intervalo = list(range(3,100,2)) #ímpares no [3, 100)
+intervalo = list(range(3,100,2)) #ímpares no intervalo [3, 100)
 
 for i in intervalo:
     maior_divisor_do_quadrado = int(math.sqrt(i))
